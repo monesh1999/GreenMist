1447ac1622f5dd81ea6310221099016af164f244
monesh1999 <r994942@gmail.com>
2025-09-08 14:55:48 +0530

    intial commit

---
+++ client/src/assets/assets.js
@@ -0,0 +1,11 @@
+import logo from './logo.jpg'
+import parcel from './parcel.jpg'
+import upload from './upload.jpg'
+import cart from './cart.jpg'
+
+export const assets = {
+    logo,
+    parcel,
+    upload,
+    cart
+};
